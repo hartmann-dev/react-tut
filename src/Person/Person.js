cb35e7c52a3c7b978935f20a82c62c2c645db5c3
Harald Hartmann <harald@hartmann-dev.net>
2018-11-19 14:01:18 +0100

    Lesson 35

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const person = () => {
-  return <p>Ich bin eine Person!</p>;
+  return (
+    <p>Ich bin eine Person und {Math.floor(Math.random() * 30)} Jahre alt!</p>
+  );
 };
 
 export default person;
