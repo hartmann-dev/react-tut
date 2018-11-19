4e3d2af209579798b08761a5fb663cb02142ec7a
Harald Hartmann <harald@hartmann-dev.net>
2018-11-19 13:53:24 +0100

    Lesson 32

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Person from "./Person/Person";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hallo</h1>
         <p>Läuft!</p>
+        <Person />
       </div>
     );
     // return React.createElement(
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = () => {
+  return <p>Ich bin eine Person!</p>;
+};
+
+export default person;
