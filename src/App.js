a17036cee8338ebf9fbb311b76b8aa7e35dd1be4
Harald Hartmann <harald@hartmann-dev.net>
2018-11-19 14:25:12 +0100

    Lesson 38

--- src/App.js
+++ src/App.js
@@ -4,16 +4,43 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Harry",
+        age: 35
+      },
+      {
+        name: "Linus",
+        age: 9
+      },
+      {
+        name: "Hilde",
+        age: 1
+      }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hallo</h1>
         <p>Läuft!</p>
-        <Person name="Harry" age="35" />
-        <Person name="Linus" age="9">
+        <button>Namensänderung</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My Hobbies: Star Wars
         </Person>
-        <Person name="Hilde" age="1" />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement(
